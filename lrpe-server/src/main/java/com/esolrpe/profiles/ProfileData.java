0a87dc034d08e4c8da45235f1ee1f9c2668f5daf
Tactitocalon <tactitocalon@gmail.com>
2016-12-08 07:30:57 +1100

    Working SQL query

--- lrpe-server/src/main/java/com/esolrpe/profiles/ProfileData.java
+++ lrpe-server/src/main/java/com/esolrpe/profiles/ProfileData.java
@@ -7,4 +7,52 @@ public class ProfileData {
     private String profileUrl;
     private boolean inactive;
     private boolean deleted;
+
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getProfileText() {
+        return profileText;
+    }
+
+    public void setProfileText(String profileText) {
+        this.profileText = profileText;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+
+    public boolean isInactive() {
+        return inactive;
+    }
+
+    public void setInactive(boolean inactive) {
+        this.inactive = inactive;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
 }
--- lrpe-server/src/main/java/com/esolrpe/profiles/ProfilesController.java
+++ lrpe-server/src/main/java/com/esolrpe/profiles/ProfilesController.java
@@ -1,19 +1,48 @@
 package com.esolrpe.profiles;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/profiles")
 public class ProfilesController {
 
-    @RequestMapping(value="getDatabaseUpdate", method = RequestMethod.GET)
-    ProfileDatabaseUpdate getDatabaseUpdate(@RequestParam(defaultValue = "0") String currentVersion) {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public ProfilesController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @RequestMapping(value="{megaserver}/getDatabaseUpdate", method = RequestMethod.GET)
+    ProfileDatabaseUpdate getDatabaseUpdate(
+            @PathVariable int megaserver,
+            @RequestParam(defaultValue = "0") String currentVersion) {
         ProfileDatabaseUpdate databaseUpdate = new ProfileDatabaseUpdate();
         databaseUpdate.setApplicationVersion(1);
         databaseUpdate.setDatabaseVersion(1337L);
+
+        int profileCount = jdbcTemplate.queryForObject(
+                "SELECT count(1) FROM profiles WHERE profiles.megaserver = ?",
+                new Object[] { megaserver },
+                Integer.class
+        );
+        databaseUpdate.setTotalProfileCount(profileCount);
+
+        List<ProfileData> profileData = jdbcTemplate.query(
+                "SELECT * FROM profiles WHERE profiles.megaserver = ?",
+                new Object[] { megaserver },
+                new BeanPropertyRowMapper<>(ProfileData.class)
+        );
+        databaseUpdate.setProfiles(profileData);
 
         return databaseUpdate;
     }
